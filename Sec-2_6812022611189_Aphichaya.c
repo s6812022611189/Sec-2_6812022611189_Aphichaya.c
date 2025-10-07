24f51eedb20e7b301353a2cf3b4cc13b29dedfe8
s6812022611189 <s6812022611189@email.kmutnb.ac.th>
2025-10-07 15:09:11 +0700

    Add files via upload

---
+++ Sec-2_6812022611189_Aphichaya.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    int i;
+    for (=3,i <=12;) {
+        printf("%d " , i);
+    }
+    printf("\nEnd of loop\n");
+    return 0;
+}
+
+//for loop: ใช้ range(15,40,20) เพื่อวนลูปตั้งแต่ 3 ถึง 12
